6a96960a667125238610138358b508b4954b007e
IODAUFPR <dashboardmarcas@gmail.com>
2024-12-16 15:00:40 -0300

    Update settings.py

--- bert_similarity_project/settings.py
+++ bert_similarity_project/settings.py
@@ -1,4 +1,5 @@
-INSTALLED_APPS = [
-    ...,
-    'similarity',  # Registrando o aplicativo
+ALLOWED_HOSTS = [
+    'localhost',  # Permite acesso do localhost
+    '127.0.0.1',  # Permite acesso do endereço IP local
+    'app.coolify.ioda.org.br',  # Permite acesso do seu domínio
 ]
