05a612f514c782e9e6b0e6879db9188ae87a878f
unit1g <116514155+unit1g@users.noreply.github.com>
2024-12-16 15:54:48 -0300

    Update settings.py

--- bert_similarity_project/settings.py
+++ bert_similarity_project/settings.py
@@ -1,5 +1,1 @@
-ALLOWED_HOSTS = [
-    'localhost',  # Permite acesso do localhost
-    '127.0.0.1',  # Permite acesso do endereço IP local
-    'app.coolify.ioda.org.br',  # Permite acesso do seu domínio
-]
+ALLOWED_HOSTS = ["*"]
